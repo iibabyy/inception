520c8979d76e09f4ac278a6eeba452657deffd80
iibabyy <utileidrissa@gmail.com>
2025-01-02 13:50:30 +0100

    fixed mariadb

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -9,13 +9,13 @@ until mysqladmin ping >/dev/null 2>&1; do
 done
 
 # log into MariaDB as root and create database and the user
-mysql  -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql  -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql  -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql  -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql  -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mysql  -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql  -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql  -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql  -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql  -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 #mysqladmin -u root shutdown
 echo "MariaDB database and user were created successfully! "
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -2,15 +2,15 @@
 
 wp_init() {
 	# 	mkdir -p /var/www/html/wordpress
-	cd /var/www/html/wordpress || exit 1
+	cd /var/www/html/wordpress
 
 	if ! wp core is-installed --allow-root; then
 
 		echo "Creating wp-config.php..."
 		wp config create --allow-root \
-			--dbname="$MYSQL_DATABASE" \
-			--dbpass="$MYSQL_PASSWORD" \
-			--dbuser="$MYSQL_USER" \
+			--dbname="$SQL_DATABASE" \
+			--dbpass="$SQL_PASSWORD" \
+			--dbuser="$SQL_USER" \
 			--dbhost="$WP_DB_HOST" \
 			--url="http://$DOMAIN_NAME";
 
@@ -34,7 +34,7 @@ wp_init() {
 
 wait_for_db() {
     echo "Waiting for database connection..."
-    until mysql -h "mariadb" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+    until mysql -h "mariadb" -u "$SQL_USER" -p"$SQL_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
         echo "Database is not ready. Retrying in 5 seconds..."
         sleep 5
     done
