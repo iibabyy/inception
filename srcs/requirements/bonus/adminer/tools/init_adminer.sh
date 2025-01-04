1a2b9fda65c879591983eb39001aa77fee19a954
ibaby <utileidrissa@gmail.com>
2025-01-04 22:57:02 +0100

    save

--- srcs/requirements/bonus/adminer/tools/init_adminer.sh
+++ srcs/requirements/bonus/adminer/tools/init_adminer.sh
@@ -1,10 +1,9 @@
 #!bin/bash
 
-wget "http://www.adminer.org/latest.php" -O /var/www/html/index.php 
+wget -q "http://www.adminer.org/latest.php" -O /var/www/html/index.php 
 chown -R www-data:www-data /var/www/html/index.php 
 chmod 755 /var/www/html/index.php
 
 cd /var/www/html
 
-# -S to run with a built-in web server
 exec php -S 0.0.0.0:80
--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
+##		FUNCTIONS		##
+
+start() {
+	sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+	mkdir -p /run/php
+
+	exec php-fpm7.4 --nodaemonize --allow-to-run-as-root
+}
+
 wp_install() {
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
-	# curl -O https://wordpress.org/wordpress-6.1.1.tar.gz
-	# tar -xzf wordpress-6.1.1.tar.gz
-	# rm wordpress-6.1.1.tar.gz
-	# mv wordpress /var/www/html/wordpress
+	wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+	chmod +x /usr/local/bin/wp
 
 	chmod -R 755 /var/www/html/wordpress/
 	chown -R www-data:www-data /var/www/html/wordpress
-
-	# find /var/www/html/wordpress/ -mindepth 1 -delete
-
-	# chown -R www-data:www-data /var/www/*
-	# chmod -R 755 /var/www/*
 }
 
 wp_init() {
 
+	wp_install
+
 	cd /var/www/html/wordpress
 
-	# if ! wp core is-installed --allow-root; then
+	echo "downloading wp..."
+	wp core download --allow-root
 
-		echo "downloading wp..."
-		wp core download --allow-root
-
-		echo "Creating wp-config.php..."
-		wp config create --allow-root \
-			--dbname="$SQL_DATABASE" \
-			--dbpass="$SQL_PASSWORD" \
-			--dbuser="$SQL_USER" \
-			--dbhost="$WP_DB_HOST" \
-			--url="http://$DOMAIN_NAME";
+	echo "Creating wp-config.php..."
+	wp config create --allow-root \
+		--dbname="$SQL_DATABASE" \
+		--dbpass="$SQL_PASSWORD" \
+		--dbuser="$SQL_USER" \
+		--dbhost="$WP_DB_HOST" \
+		--url="http://$DOMAIN_NAME";
 
 
-		echo "Installing WordPress..."
-		wp core install --allow-root \
-			--title="$SITE_TITLE" \
-			--admin_user="$ADMIN_USER" \
-			--admin_password="$ADMIN_PASSWORD" \
-			--admin_email="$ADMIN_EMAIL" \
-			--url="http://$DOMAIN_NAME" \
-			--skip-email;
+	echo "Installing WordPress..."
+	wp core install --allow-root \
+		--title="$SITE_TITLE" \
+		--admin_user="$ADMIN_USER" \
+		--admin_password="$ADMIN_PASSWORD" \
+		--admin_email="$ADMIN_EMAIL" \
+		--url="http://$DOMAIN_NAME" \
+		--skip-email;
 
-		echo "Creating new user..."
-			wp user create --allow-root \
-			"$USER_LOGIN" "$USER_EMAIL" \
-			--role=author \
-			--user_pass="$USER_PASSWORD";
-	# fi
+	echo "Creating new user..."
+		wp user create --allow-root \
+		"$USER_LOGIN" "$USER_EMAIL" \
+		--role=author \
+		--user_pass="$USER_PASSWORD";
+
+	wp plugin install redis-cache --activate --allow-root
 
 }
 
@@ -63,18 +63,10 @@ wait_for_db() {
     echo "Database is ready!"
 }
 
-sleep 5
+
+
+##		Starting	##
+
 wait_for_db
-
-echo "[========WP INSTALLATION STARTED========]"
-
-wp_install
-sleep 5
 wp_init
-
-echo "[=======WP INSTALLATION FINISHED=========]"
-
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-mkdir -p /run/php
-
-exec php-fpm7.4 --nodaemonize --allow-to-run-as-root
+start
